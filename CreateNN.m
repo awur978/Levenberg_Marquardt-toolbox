0485ac3dfb3085f8cf8bc85322d435daba7b1c0c
Adedamola Wuraola <32375311+awur978@users.noreply.github.com>
2018-09-21 12:35:54 +1200

    Minor Changes

--- CreateNN.m
+++ CreateNN.m
@@ -1,17 +1,16 @@
-% Netzaufbau mit Gewichten und Verzögerungen erstellen und wichtige ngen
-% erstellen
+% Create network structure with weights and delays and attach important ones Create %
 
 function [net]=CreateNN(nn,dIn,dIntern,dOut)
 
-% dIn: Zelle für jeden Eingang mit Vektor der Verzögerungen
+% dIn: cell for each input with vector of delays
 if ~exist('dIn', 'var')
     dIn = [0];
 end
-% dIntern: Vektor mit den Verzögerungen der Internen Schichten (AußerSM -> S1)
+% dIntern: Vector with the delays of the internal layers (except SM -> S1)
 if ~exist('dIntern', 'var')
     dIntern = [];
 end
-% dOut: Zelle für jeden Ausgang mit Vektor der Verzögerungen zur Schicht S1
+% dOut: cell for each output with delay line vector S1
 if ~exist('dOut', 'var')
     dOut = [];
 end
@@ -20,24 +19,25 @@ end
 
 
 
-% NN=[P S1 S2 .. SM] Aufbau des Netzes( P steht für Anzahl der
-    % Eingänge, Sm für die Anzahl der ronen von Schicht m
+%  NN = [P S1 S2 .. SM] Structure of the network (P stands for number of
+    % Inputs, Sm for the number of rons of layer m
+
 
     
   
-net.nn=nn; %Gesamtaufbau mit Eingängen
-net.delay.In=dIn;    %Eingansverzögerung
-net.delay.Intern=dIntern;    %Interne Verzögerungen
-net.delay.Out=dOut;    %Ausgangsverzögerungen
+net.nn=nn; %Overall construction with inputs
+net.delay.In=dIn;    % Input delay
+net.delay.Intern=dIntern;    %Internal delays
+net.delay.Out=dOut;    %Exit delays
 
-net.M=length(nn)-1;        %Anzahl der Schichten des Neuronalen Netzes
-net.layers=nn(2:end);      % Aufbau des NN ohne Eingangsmatrix [S1 S2...SM]
-net.dmax=max([net.delay.In,net.delay.Intern,net.delay.Out]); %Maximale Verzögerung innerhalb des NN
+net.M=length(nn)-1;        % Number of layers of the neural network
+net.layers=nn(2:end);      % Structure of NN without input matrix [S1 S2 ... SM]
+net.dmax=max([net.delay.In,net.delay.Intern,net.delay.Out]); %Maximum delay within the NN
 
 
-[net]=w_Create(net); %Gesamtgewichtsvektor Net.w_0 erzeugen sowie wichtige Mengen erzeugen
+[net]=w_Create(net); %Generate total weight vector Net.w_0 and generate significant quantities
 
-net.N=length(net.w_0); %Anzahl der Gewichte
-net.w=net.w_0; %Zu Otimierender Gesamtgewichtsvektor Net.w_k ist zu Beginn der Startvektor Net.w_0
+net.N=length(net.w_0); %Number of weights
+net.w=net.w_0; %Net.w_k to be initialized is the start vector Net.w_0 at the beginning
 
 end
--- train_LM.m
+++ train_LM.m
@@ -1,4 +1,4 @@
-%Levenberg Marquardt (LM) mit Jakobimatrixberechnung durch RTRL
+% Levenberg Marquardt (LM) with Jacobian matrix calculation by RTRL file
 
 function net=train_LM(P,Y,net,k_max,E_stop)
 
@@ -7,56 +7,56 @@ dampfac    =     3;    %damping factor of LM
 
 [data,net] = prepare_data(P,Y,net);
 
-[J,E,e]=RTRL(net,data);  %Jakobimatrix, Kostenfunktion und Fehlervektor des Startvektors  berechnen
+[J,E,e]=RTRL(net,data);  %Calculate Jacobian matrix, cost function and error vector of the start vector
 
-k=1; %Erster Iterationsschritt
-Ek(k)=E; %Verlauf der Kostenfunktion
-disp(['Iteration: ', num2str(k),'   Error: ', num2str(E),'   SkalFakt:', num2str(dampfac)])
+k=1; %First iteration step
+Ek(k)=E; %Course of the cost function
+disp(['Iteration: ', num2str(k),'   Error: ', num2str(E),'   Damping Factor:', num2str(dampfac)])
 
 while 1
 
-    JJ=J'*J; %J(transp) mal J
-    w=net.w;    %Aktuellen Gewichtsvektor speichern
+    JJ=J'*J; %J (transp) * J
+    w=net.w;    % Save current weight vector
     
-    while 1 %Bis Optimierungsschritt erfolgreich:
+    while 1 %Until optimization step successful:
         
-%         G=inv(JJ+SkalFakt.*eye(size(JJ,1))); %Skalierte Inverse Hessematrix berechnen
+%         G=inv(JJ+SkalFakt.*eye(size(JJ,1))); %Calculate Scaled Inverse Hessematrix
          G=(JJ+dampfac.*eye(size(JJ,1)))\eye(size(JJ+dampfac.*eye(size(JJ,1))));
         
         g=J'*e;  %Gradient
         if isnan(G(1,1))
             w_delta=-1/1e10.*g;
         else
-            w_delta=-G*g;  %Gewichtsänderung ermitteln: w_delta=-G*g
+            w_delta=-G*g;  %Determine change in weight: w_delta = -G * g
         end
-        net.w=w+w_delta; %Gewichte anpassen
+        net.w=w+w_delta; %Adjust weights
     
-        [E2] = calc_error(net,data); %Kostenfunktion an neuem Gewichtsvektor berechnen
+        [E2] = calc_error(net,data); %Calculate cost function on new weight vector
 
-        %-----------Optimierungsschritt erfolgreich------------------------
+        %-----------Optimization step successful------------------------
         if E2<E     
-            dampfac=dampfac/dampconst;    %Skalierungsfaktor anpassen
-            break;                          %Weiter
+            dampfac=dampfac/dampconst;    %Adjust scaling factor
+            break;                          %Next
             
-        %---------%Optimierungsschritt NICHT erfolgreich--------------------   
+        %---------%Optimization step NOT successful--------------------   
         elseif E2>=E    
-            dampfac=dampfac*dampconst;     %Skalierungsfaktor anpassen       
+            dampfac=dampfac*dampconst;     %Adjust % scaling factor        
         end          
     end
     
-    [J,E,e,a]=RTRL(net,data);  %Jakobimatrix, Kostenfunktion und Fehlervektor an neuem Gewichtsvektor berechnen berechnen
+    [J,E,e,a]=RTRL(net,data);  %Jacobian matrix, calculate cost function and error vector to calculate new weight vector
     
     
-    k=k+1;  %Aktueller Iterationsschritt
-    Ek(k)=E; %Verlauf der Kostenfunktion
-    disp(['Iteration: ', num2str(k),'   Error: ', num2str(E),'   SkalFakt:', num2str(dampfac)])
+    k=k+1;  %Current iteration step
+    Ek(k)=E; %Course of the cost function
+    disp(['Iteration: ', num2str(k),'   Error: ', num2str(E),'   Damping Factor:', num2str(dampfac)])
 
-    if (k>=k_max) || (E<=E_stop) % Abbruch wenn eines der Abbruchkriterien erfüllt 
+    if (k>=k_max) || (E<=E_stop) % abort if one of the abort criteria is met
            break
     end    
     
 end
 
-%Ausgabe
+%Output
 net.ErrorHistory=Ek;
 
